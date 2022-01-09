828a6104f5da7c09bd4e997064e0cbc396d70566
Erick Escagion <erickeescagion@gmail.com>
2022-01-09 12:45:49 -0300

    config documentation

--- src/main/java/dio/person_api/controllers/PersonController.java
+++ src/main/java/dio/person_api/controllers/PersonController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 
 @RestController
@@ -13,8 +16,12 @@ import lombok.AllArgsConstructor;
 public class PersonController {
 
     @GetMapping
+    @Operation(summary = "GET Person", description = "Route to see all registered person", tags = "Person")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "success doing GET Person"),
+    })
     public String listAll() {
         return "teste API";
     }
-    
+
 }
