97b7c2d76c4e5c9a5f4f7ec8b051dacd0f42a893
Erick Escagion <erickeescagion@gmail.com>
2022-01-10 14:19:25 -0300

    creating PersonService (POST)

---
+++ src/main/java/dio/person_api/services/PersonService.java
@@ -0,0 +1,37 @@
+package dio.person_api.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import dio.person_api.dto.mapper.PersonMapper;
+import dio.person_api.dto.request.PersonDTO;
+import dio.person_api.dto.response.MessageResponseDTO;
+import dio.person_api.entities.Person;
+import dio.person_api.repositories.PersonRepository;
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class PersonService {
+    
+
+    private final PersonRepository personRepository;
+
+    private final PersonMapper personMapper;
+    
+    public MessageResponseDTO create(PersonDTO personDTO) {
+        Person person = personMapper.toModel(personDTO);
+        Person savedPerson = personRepository.save(person);
+
+        MessageResponseDTO messageResponse = createMessageResponse("Person successfully created with ID ", savedPerson.getId());
+
+        return messageResponse;
+    }
+
+    private MessageResponseDTO createMessageResponse(String s, Long id2) {
+        return MessageResponseDTO.builder()
+                .message(s + id2)
+                .build();
+    }
+    
+}
