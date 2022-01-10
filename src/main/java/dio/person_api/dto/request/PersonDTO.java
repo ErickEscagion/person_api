ac0ee51a9fdae3acb1da68d6bbd8c2499e7d9b26
Erick Escagion <erickeescagion@gmail.com>
2022-01-10 14:17:42 -0300

    creating personDTO

---
+++ src/main/java/dio/person_api/dto/request/PersonDTO.java
@@ -0,0 +1,35 @@
+package dio.person_api.dto.request;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PersonDTO {
+
+    private Long id;
+
+    @NotEmpty
+    @Size(min = 2, max = 100)
+    private String firstName;
+
+    @NotEmpty
+    @Size(min = 2, max = 100)
+    private String lastName;
+
+    @NotEmpty
+    @CPF
+    private String cpf;
+
+    @NotNull
+    private String birthDate;
+}
