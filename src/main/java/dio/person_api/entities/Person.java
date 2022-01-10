0d5bc245c4ff7fd5c12781869513ce8f15e8fdf7
Erick Escagion <erickeescagion@gmail.com>
2022-01-10 14:17:16 -0300

    creating person entity

---
+++ src/main/java/dio/person_api/entities/Person.java
@@ -0,0 +1,36 @@
+package dio.person_api.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    private LocalDate birthDate;
+}
