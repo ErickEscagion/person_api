41566aef50ce4baf6dc734692d03692fccf83ab7
Erick Escagion <erickeescagion@gmail.com>
2022-01-10 15:18:41 -0300

    configuring h2 and entering person data

--- src/main/java/dio/person_api/entities/Person.java
+++ src/main/java/dio/person_api/entities/Person.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,6 +19,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name="TB_PERSON")
 public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
